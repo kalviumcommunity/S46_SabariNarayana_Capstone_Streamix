4dfb747d9011574dddc53751b75f8cd12496ee1b
Sabari Narayana <sabarinarayana@Sabaris-MacBook-Air.local>
2024-03-18 11:53:08 +0530

    file setup done for homepage

---
+++ Frontend/src/components/navBar/HomeNavBar.jsx
@@ -0,0 +1,8 @@
+ import React from 'react'
+ 
+ export const HomeNavBar = () => {
+   return (
+     <div className='text-black border-2 border-black w-full'><nav></nav></div>
+  )
+ }
+ 
---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import {HomeNavBar} from "../components/navBar/HomeNavBar"
+
+export const Home = () => {
+  return (
+    <div><HomeNavBar/></div>
+  )
+}
