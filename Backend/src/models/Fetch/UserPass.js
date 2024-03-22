cf6f95bbd9bdbadb545c8a9c26fe9c3b9c078eb1
Sabari Narayana <sabarinarayana@Sabaris-MacBook-Air.local>
2024-03-22 11:39:25 +0530

    file added

---
+++ Backend/src/models/Fetch/UserPass.js
@@ -0,0 +1,22 @@
+const { UserModel } = require('../Schema')
+
+const FetchUserPass = async (email) => {
+    try {
+        const user = await UserModel.findOne({ email })
+        if (user) {
+            return { email: user.email, password: user.password }
+        } else {
+            return { UserExist: false }
+        }
+    } catch (error) {
+        // Handle error
+        console.error('Error fetching email and password:', error.message)
+        // Return false in case of error
+        return false
+    }
+}
+
+// Exporting functions and router for use in other modules
+module.exports = {
+    FetchUserPass,
+}
---
+++ Backend/src/utils/passwordChecker.js
@@ -0,0 +1,12 @@
+const bcrypt = require('bcrypt')
+
+const passwordChecker = async (givenPass, FetchPass) => {
+    const match = await bcrypt.compare(givenPass, FetchPass)
+    console.log(match)
+    return match
+}
+
+// Exporting functions and router for use in other modules
+module.exports = {
+    passwordChecker,
+}
