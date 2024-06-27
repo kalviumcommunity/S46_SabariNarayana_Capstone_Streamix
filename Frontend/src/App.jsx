3f9cb518fa1f336b106e6ff21edf7e8bf1920bd6
Melvin Kannan <139456150+Melvin0070@users.noreply.github.com>
2024-06-27 17:04:04 +0000

    organized app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,48 +3,49 @@ import { Navigate, Outlet, Route, Routes } from 'react-router-dom';
 import { useSignals } from '@preact/signals-react/runtime';
 import { userExists } from '@/signals/user.js';
 import { HomeNav } from '@/components/NavBar/HomeNav';
+import { ProdNav } from '@/components/NavBar/ProdNav';
 import { Dashboard } from '@/pages/Product/Dashboard';
 import { Home } from '@/pages/Home/Home';
 import { SignIN } from '@/pages/Auth/SignIN';
 import { SignUP } from '@/pages/Auth/SignUP';
-import { ProdNav } from '@/components/NavBar/ProdNav';
-import { Notification } from './pages/Product/Notification';
-import { Report } from './pages/Product/Report';
-import { Setting } from './pages/Product/Setting';
-import { UploadVideo } from './pages/Product/UploadVideo';
+import { Notification } from '@/pages/Product/Notification';
+import { Report } from '@/pages/Product/Report';
+import { Setting } from '@/pages/Product/Setting';
+import { UploadVideo } from '@/pages/Product/UploadVideo';
 
 function Layout() {
+  const navComponent = userExists.value ? <ProdNav /> : <HomeNav />;
   return (
-    <div className={userExists.value ? 'flex  h-full' : 'h-full'}>
-      {userExists.value ? <ProdNav /> : <HomeNav />}
+    <div className={userExists.value ? 'flex h-full' : 'h-full'}>
+      {navComponent}
       <Outlet />
     </div>
   );
 }
 
 function App() {
-  console.log('Rendering the App component');
+  console.log('Rendering App component');
   useSignals();
+
   return (
     <div className="w-screen h-full max-w-">
       <Routes>
         <Route path="/" element={<Layout />}>
-          {userExists.value === true ? (
+          {userExists.value ? (
             <>
               <Route index element={<Dashboard />} />
-              <Route path="/notification" element={<Notification />} />
-              <Route path="/report" element={<Report />} />
-              <Route path="/setting" element={<Setting />} />
-              <Route path="/upload_video" element={<UploadVideo />} />
-              <Route path="*" element={<Navigate to="/" replace={true} />} />
+              <Route path="notification" element={<Notification />} />
+              <Route path="report" element={<Report />} />
+              <Route path="setting" element={<Setting />} />
+              <Route path="upload_video" element={<UploadVideo />} />
+              <Route path="*" element={<Navigate to="/" replace />} />
             </>
           ) : (
             <>
               <Route index element={<Home />} />
-              <Route path="/signin" element={<SignIN />} />
-              <Route path="/singup" element={<SignUP />} />
-              {/* <Route path="/read" element={<Navigate to="/login" replace={true} />} /> */}
-              <Route path="*" element={<Navigate to="/" replace={true} />} />
+              <Route path="signin" element={<SignIN />} />
+              <Route path="signup" element={<SignUP />} />
+              <Route path="*" element={<Navigate to="/" replace />} />
             </>
           )}
         </Route>
